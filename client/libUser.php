a8a30ec7e6668f594271f5c3633e349eb20c31ac
Duane Morris <duanem027@gmail.com>
2017-09-16 08:06:10 -0700

    Created User class base file

---
+++ client/libUser.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author: Duane Morris duanem027@gmail.com
+ * @version 0.1
+ *
+ * User Class for workflow sessions
+ *
+*/
+
+namespace Apps\Lib\Users
+
+class userInfo {
+    private userName='';
+    private userAlias='';
+    
+    public function __construct(){
+    }
+
+    public function __destruct(){
+    }
+
+
+}
+?>
